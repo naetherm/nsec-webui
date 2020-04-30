220d527d7a2f8025e12c5a8f64cd60922434a337
naetherm <naether.markus@gmail.com>
2020-04-30 15:37:35 +0200

    * Fixes for the docker file

--- app/Http/Controllers/BackendController.php
+++ app/Http/Controllers/BackendController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -11,21 +12,7 @@ class BackendController extends Controller
 
 
     public function postRequest(Request $request) {
-        //dd($request->json()->all());
-        /*
-         *
-                'text' => $request->json()->get('content'),
-                'detector' => $request->json()->get('detection'),
-                'suggestor' => $request->json()->get('suggestion'),
-                'ranker' => $request->json()->get('ranking')
-         */
-        //echo $request->json()->all();
-
-        //dd($request->json());
-
-        $response = Http::post('http://0.0.0.0:9875/', [
-            'name' => 'kakadu',
-            'req' => $request->all(),
+        $response = Http::post('nsec:9875/', [
             'text' => $request->input('content'),
             'detector' => $request->input('detection'),
             'suggestor' => $request->input('suggestion'),
@@ -33,29 +20,10 @@ class BackendController extends Controller
         ]);
 
         return $response->json();
-        dd($response->json());
-        /*
-
-        $client = new \GuzzleHttp\Client();
-        $response = $client->request('GET', 'http://0.0.0.0:9876/', [
-            'form_params' => [
-                'text' => $request->json()->get('content'),
-                'detector' => $request->json()->get('detection'),
-                'suggestor' => $request->json()->get('suggestion'),
-                'ranker' => $request->json()->get('ranking')
-            ]
-          ]
-        );
-        $response = $response->getBody()->getContents();
-        //print_r($request->json()->all());
-        echo '<pre>';
-        print_r($response);
-        return $response->json();
-        */
     }
     public function getRequest() {
-        $client = new \GuzzleHttp\Client();
-        $request = $client->get('http://0.0.0.0:9875/');
+        $client = new Client();
+        $request = $client->get('nsec:9875/');
         $response = $request->getBody()->getContents();
         echo '<pre>';
         print_r($response);
@@ -63,7 +31,7 @@ class BackendController extends Controller
     }
 
     public function postBenchmarkRequest(Request $request) {
-        $response = Http::post('http://0.0.0.0:9875/bench', [
+        $response = Http::post('nsec:9875/bench', [
             'benchmark' => $request->input('benchmark')
         ]);
 
